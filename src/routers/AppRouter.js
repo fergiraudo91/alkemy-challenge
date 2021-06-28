eb7e01784dc2d6eedceb8cd071dfc56e51650272
fergiraudo91 <fergiraudo91@gmail.com>
2021-06-28 18:31:26 -0300

    fixing issues on routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,13 +4,14 @@ import { AuthContext } from "../auth/AuthContext";
 import { LoginScreen } from "../components/login/LoginScreen";
 import { DashboardRoutes } from "./DashboardRoutes";
 import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
 
 export const AppRouter = () => {
   const { user } = useContext(AuthContext);
   return (
     <Router>
         <Switch>
-          <Route exact path="/login" component={LoginScreen} />
+          <PublicRoute  path="/login" component={LoginScreen} isAuthenticated={user.logged} />
           <PrivateRoute path="/" component={DashboardRoutes} isAuthenticated={user.logged} />
         </Switch>
     </Router>
